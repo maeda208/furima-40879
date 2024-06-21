2b1a1ff1e9478d69a1fd10fd83d74169af37ba68
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-06-21 17:21:55 +0900

    動作確認

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,7 +14,7 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(item_id:item.id, user_id:user.id)
+    order = Order.create(item_id:item_id, user_id:user_id)
 
     Address.create(post_code:post_code, prefecture_id:prefecture_id, municipalities:municipalities, address:address,
                    building_name:building_name, phone_number:phone_number, order_id:order.id)
